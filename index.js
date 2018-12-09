f11ceb58bf7542799187b4ed3f62214934c0e348
noob.dev6.naver.cloud <noob.dev6@gmail.com>
2018-12-09 23:22:08 +0900

    Hello World!

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+var express = require('express'); // 설치한 express module을 불러와서 변수 express에 담습니다
+var app = express(); // express를 실행하여 app object를 초기화 합니다.
+
+app.get('/', function(req, res) { // '/' 위치에 'get' 요청을 받는 경ㅇ,
+	res.send('Hello World!');
+});
+
+app.listen(12345, function() { // 12345번 포트를 사용합니다.
+	console.log('Server On!');
+});
